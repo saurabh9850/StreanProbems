45b00870967b00120b9e7fabea831144adc2b127
saurabh9850 <saurabhchavan9850@gmail.com>
2025-05-01 01:49:41 +0530

    Update ArraySortingByStream.java

--- StreamProblems/ArraySortingByStream.java
+++ StreamProblems/ArraySortingByStream.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class ArraySortingByStream {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int a[]= {20,45,39,76,98};
 		
 		int b[]=Arrays.stream(a)
